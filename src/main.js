23d8cd57e835bdf887ed2044137b1b3d151507af
Charley McGowan and Michael Brown <michael@>
2019-01-07 13:32:15 -0800

    negative match succefully works.

--- src/main.js
+++ src/main.js
@@ -18,7 +18,19 @@ $(document).ready(function() {
       hot.push(new Card($(this).attr('id')));
     });
 
-    console.log(card)
+    console.log(card);
+    if (hot.length > 1) {
+      if (hot[0].isMatch(hot[1])) {
+        $('.just-flipped').addClass('flipped');
+        $('.just-flipped').removeClass('just-flipped');
+      } else {
+        // add a couple of seconds
+        $('.just-flipped').addClass('unflipped');
+        $('.just-flipped').removeClass('just-flipped');
+      }
+    }
+
+
     console.log("hot card ids: " + hot);
 
   });
